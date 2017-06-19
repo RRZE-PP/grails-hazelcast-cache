3c977047308b27b6ae31221319d21cd05ae31007
ma33fyza <anthony.bach@fau.de>
2017-06-19 10:58:59 +0200

    imports fixed and @Slf4j removed from HazelcastPageFragmentCacheFilter

--- src/main/groovy/org/grails/plugins/hazelcast/cache/HazelcastPageFragmentCacheFilter.groovy
+++ src/main/groovy/org/grails/plugins/hazelcast/cache/HazelcastPageFragmentCacheFilter.groovy
@@ -1,26 +1,12 @@
 package org.grails.plugins.hazelcast.cache
 
-import grails.plugin.cache.GrailsValueWrapper
-import grails.plugin.cache.web.AlreadyGzippedException
-import grails.plugin.cache.web.Header
 import grails.plugin.cache.web.PageInfo
 import grails.plugin.cache.web.filter.PageFragmentCachingFilter
-import groovy.util.logging.Slf4j
-import org.apache.commons.lang.builder.EqualsBuilder
-import org.apache.commons.lang.builder.HashCodeBuilder
 import org.springframework.cache.Cache
-
-import javax.servlet.FilterChain
-import javax.servlet.ServletException
-import javax.servlet.http.Cookie
-import javax.servlet.http.HttpServletRequest
-import javax.servlet.http.HttpServletResponse
-import java.util.concurrent.TimeUnit
 
 /**
  * Created by ma33fyza on 14.06.17.
  */
-@Slf4j
 class HazelcastPageFragmentCacheFilter extends PageFragmentCachingFilter {
 
 
